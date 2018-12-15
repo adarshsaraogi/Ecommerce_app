f67e194ea1754085b1c9c6efdca52ea348c1c114
adarshsaraogi <43959879+adarshsaraogi@users.noreply.github.com>
2018-12-16 01:05:34 +0530

    delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-var server = http.createServer((req,res)=>{
-    return res.end ("hello world");
-})
-server.listen(2000,()=>{console.log("server listening to port 2000");});
